c9420526a555333eb1e7062ca3686b032a52bbbb
Sander Janssens <sander.janssens@outlook.be>
2021-09-15 17:41:45 +0200

    Refactor: Extracted the methods and could get rid of the Temp variables by replacing them with Queries

--- src/main/java/com/rentalservice/Customer.java
+++ src/main/java/com/rentalservice/Customer.java
@@ -26,25 +26,38 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = "Rental Record for " + name + "\n";
         Iterator<Rental> it = rentalList.iterator();
         while (it.hasNext()) {
 
             Rental each = it.next();
 
-            //determine amounts for each line
-
-            frequentRenterPoints = each.getFrequentRenterPoints();
-
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
-            totalAmount += each.getCharge();
         }
         //add Footer lines
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points.";
+        result += "Amount owed is " + getTotalCharge() + "\n";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points.";
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Iterator<Rental> it = rentalList.iterator();
+        while (it.hasNext()) {
+            Rental each = it.next();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Iterator<Rental> it = rentalList.iterator();
+        while (it.hasNext()) {
+            Rental each = it.next();
+            result += each.getCharge();
+        }
         return result;
     }
 
